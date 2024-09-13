dd28b1d726882856fa9ca1dbca51cb6f13bfd592
Andre-Souza539 <aljs.junca@gmail.com>
2024-09-13 14:09:33 -0300

    Criação dos primeiros Endpoints da aplicação -teste -contact -página principal -about

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ import 'dotenv/config'
 const app = express()
 const port = process.env.PORT
 
+app.get("/", (req,res)=> {
+    res.send("<p> Página inicial do projeto </p>")
+});
+
+app.get("/contact", (req,res)=>{
+    res.send("<p>Página de contato</p><h1>Entre em Contato</h1><p>+55 (11) 99999-9999</p>")
+});
+
+app.get("/about", (req,res)=>{ 
+    res.send("<p>Página de Sobre</p>")
+});
+
+app.get("/teste", (req, res) => {
+    res.send("<h1>Teste</h1>");
+});
+
 app.listen(port, ()=>{
     console.log(`Server Running at http://localhost:${port}`);
 })
