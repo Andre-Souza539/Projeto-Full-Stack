74c6037819d2e479a49e8a7ce4a90f6b697c1f8c
Andre-Souza539 <aljs.junca@gmail.com>
2024-09-16 21:30:38 -0300

    Criação do Middleware autenticador() que verifica se o usuário está logado com base em uma simples verificação de usuário e senha padrão

--- index.js
+++ index.js
@@ -13,11 +13,41 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan("common"));
 
+var autenticado = false
+
+function autenticador(req, res, next){
+    const username = req.body["username"];
+    const password = req.body["password"];
+    if(username === "admin" && password === "admin"){
+        autenticado = true
+    }
+    next();
+}
+
+app.use(autenticador);
 
 app.get("/", (req,res)=> {
     console.log(req.body);
     res.sendFile(__dirname + "/public/view/index.html");
 });
+
+app.get("/login", (req,res) => {
+    res.sendfile(__dirname + "/public/view/login.html");
+})
+
+app.post("/check", (req,res)=> {
+    if(autenticado){
+        res.sendFile(__dirname + "/public/view/logado.html");
+    } else { 
+        res.send("Deixa de ser tabacudo o usuário e senha é admin")
+    }
+})
+
+app.get("/deslogar", (req,res)=> {
+    autenticado = false;
+    res.sendFile(__dirname + "/public/view/login.html")
+})
+
 
 app.get("/contact", (req,res)=>{
     res.send("<p>Página de contato</p><h1>Entre em Contato</h1><p>+55 (11) 99999-9999</p>")
